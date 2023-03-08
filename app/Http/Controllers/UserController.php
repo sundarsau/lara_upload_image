cf42aec76d11ed81c6deb0faebe5e1bce28bb435
sundarsau <sundarsau@gmail.com>
2023-03-08 12:22:02 +0530

    some fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\File;
 
